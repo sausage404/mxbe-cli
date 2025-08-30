90a3112e3c9a088f1781eb4ce2882f2fd63f9fb7
sausage404 <parinya24personal@gmail.com>
2025-08-30 16:00:40 +0700

    update

--- src/cmd/link.ts
+++ src/cmd/link.ts
@@ -1,32 +1,33 @@
 import fs from "fs-extra";
 import path from "path";
 import os from "os";
-import dotenv from "dotenv";
 import common from "@mxbe/common";
+import yml from "js-yaml";
 
 export default async () => {
     const projectPath = process.cwd();
 
     const pkg = await common.getJsonFile.package();
 
-    dotenv.config({ path: path.join(projectPath, ".env") });
+    const rawYaml = fs.readFileSync(path.join(projectPath, "paths.yml"), "utf-8");
+    const paths = yml.load(rawYaml) as { base_path: string, resource_path: string, behavior_path: string };
 
     const mcDir = path.join(
         os.homedir(),
-        process.env.BASE_PATH
+        paths.base_path
     );
 
-    const bpSrc = path.join(projectPath, "behavior");
     const rpSrc = path.join(projectPath, "resource");
+    const bpSrc = path.join(projectPath, "behavior");
 
-    const bpDest = path.join(mcDir, process.env.BEHAVIOR_PATH, pkg.name);
-    const rpDest = path.join(mcDir, process.env.RESOURCE_PATH, pkg.name);
+    const rpDest = path.join(mcDir, paths.resource_path, pkg.name);
+    const bpDest = path.join(mcDir, paths.behavior_path, pkg.name);
 
-    if (fs.existsSync(bpDest)) fs.removeSync(bpDest);
     if (fs.existsSync(rpDest)) fs.removeSync(rpDest);
+    if (fs.existsSync(bpDest)) fs.removeSync(bpDest);
 
-    if (fs.existsSync(bpSrc)) fs.symlinkSync(bpSrc, bpDest, "junction");
     if (fs.existsSync(rpSrc)) fs.symlinkSync(rpSrc, rpDest, "junction");
+    if (fs.existsSync(bpSrc)) fs.symlinkSync(bpSrc, bpDest, "junction");
 
     console.log(`Linked ${pkg.name} to ${mcDir}`);
 }
